0a21dda3316d9a4156b7f244d30d5c4c4eac347b
alaaahmade <alaaahmade66@gmail.com>
2023-05-13 01:19:07 +0300

    fix deploy

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 app.set('PORT', 9000)
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'src', 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'build', 'index.html'));
 });
 
 app.listen(app.get('PORT'), () => {
